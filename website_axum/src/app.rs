25032a61d714066a3146566baf57f354c7502d06
Xanton <4190090+Xanton@users.noreply.github.com>
2025-01-18 12:54:58 +0100

    Update app.rs
    Add new Syntax Highlighting

--- website_axum/src/app.rs
+++ website_axum/src/app.rs
@@ -113,6 +113,10 @@ pub fn App() -> impl IntoView {
                 </Routes>
             </Layout>
         </Router>
+        // add syntax highlighting here: 
+        <script src="/highlight/highlight.min.js"></script>
+        <link rel="stylesheet" href="/highlight/styles/default.css"/>
+        <script>hljs.highlightAll();</script>
         </body>
     }
 }
